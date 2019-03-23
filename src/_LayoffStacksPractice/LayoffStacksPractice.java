19e571d3d6a175e8b608cd4fa07a2b46f996e041
tactreconx <league@WTS-MM-11.local>
2019-03-23 11:25:01 -0700

    Created layoffStacks class

---
+++ src/_LayoffStacksPractice/LayoffStacksPractice.java
@@ -0,0 +1,14 @@
+package _LayoffStacksPractice;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LayoffStacksPractice {
+	private JFrame frame = new JFrame();
+	private JPanel panel = new JPanel();
+	private JButton hireButton = new JButton();
+	private JButton layoffButton = new JButton();
+	private JLabel nameLabel = new JLabel();
+}
